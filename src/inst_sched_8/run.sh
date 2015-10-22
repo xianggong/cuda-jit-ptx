7a89d463d3b3306c6fdc47ad5137f9b203f61bf4
Xiang Gong <xgong@ece.neu.edu>
2015-10-22 16:10:16 -0400

    * inst_sched_8 added

---
+++ src/inst_sched_8/run.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+# Profiling file name
+fileName=$(date +%H_%M_%S_%Y)
+
+# Executable file name
+execName=inst_sched_8
+
+# Read global variables
+maxOffset=$1
+maxAluDistance=$2
+maxMemDistance=$3
+
+# Check number of parameters
+if [ "$#" -ne 3 ]; then
+  echo "Usage: $0 maxOffset maxAluDistance maxMemDistance"
+  exit 1
+fi
+
+# Generate files
+genFile () {
+  for((aluDist=1;aluDist<=$maxAluDistance;aluDist+=1))
+  do
+    for((memDist=1;memDist<=$maxMemDistance;memDist+=1))
+    do
+      python gen_kernel.py $aluDist $memDist
+    done
+  done
+}
+
+# Profiling
+profile () {
+  aluDist=$1
+  memDist=$2
+  for ((blockSize=32;blockSize<=1024;blockSize*=2))
+  do
+    maxScale=$((1024*32760/${blockSize}))
+    echo "$scale" >> ${fileName}.txt
+#   echo "$maxScale"
+    for ((scale=1;scale<${maxScale};scale*=2))
+    do
+#     echo "scale = $scale"
+      dataCount=$(($blockSize*15))
+#     echo "dataCount = $dataCount"
+#     echo "blockSize = $blockSize"
+      for((offset=0;offset<=$maxOffset;offset+=1))
+      do
+        k0=$(nvprof ./${execName} $dataCount $scale $offset $blockSize kernel0_${aluDist}_${memDist}.ptx 2>&1 | grep '  kernel' | awk '{print $2}' 2>&1)
+        k1=$(nvprof ./${execName} $dataCount $scale $offset $blockSize kernel1_${aluDist}_${memDist}.ptx 2>&1 | grep '  kernel' | awk '{print $2}' 2>&1)
+        k2=$(nvprof ./${execName} $dataCount $scale $offset $blockSize kernel2_${aluDist}_${memDist}.ptx 2>&1 | grep '  kernel' | awk '{print $2}' 2>&1)
+#  echo "k0/1/2 $k0 $k1 $k2"
+    k0Value=$(echo $k0 | grep -o "[0-9]*\.[0-9]*")
+    k1Value=$(echo $k1 | grep -o "[0-9]*\.[0-9]*")
+    k2Value=$(echo $k2 | grep -o "[0-9]*\.[0-9]*")
+# echo "scale=10; $k0Value/$k1Value*100"
+# echo "scale=10; $k0Value/$k2Value*100"
+    speedup01=$(echo "scale=10; $k0Value/$k1Value*100" | bc)
+    speedup02=$(echo "scale=10; $k0Value/$k2Value*100" | bc)
+    echo "$aluDist $memDist $dataCount $scale $offset $k0 $k1 $k2 $speedup01% $speedup02%" >> ${fileName}.txt
+    done
+  done
+done
+}
+
+####### Main
+
+genFile
+
+for((aluDist=1;aluDist<=$maxAluDistance;aluDist+=1))
+do
+  for((memDist=$aluDist;memDist<=$maxMemDistance;memDist+=1))
+  do
+    profile $aluDist $memDist
+  done
+done
+
